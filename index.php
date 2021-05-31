d9d16e4146d137f100775a980c043c66fa69ae35
Sarah89Pro <sarahproietti1@gmail.com>
2021-05-31 14:24:02 +0200

    movie data e index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+require_once __DIR__ . '/movie.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Oop 1</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Little Movie DB</h1>
+    </header>
+
+    <main>
+        <section class="movies">
+
+        </section>    
+    </main>
+</body>
+</html>
---
+++ movie-data.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * Sorgente Dati
+ */
+
+ $movies = [
+    [
+        'title' =>'Crudelia',
+        'distribuited_by' =>'Walt Disney',
+        'director' =>'Craig Gillespie',
+        'plot' =>'Estella è una bambina con un sogno e un lato oscuro: il sogno è quello di diventare una celebre stilista di moda,
+        il lato oscuro è il desiderio di sfidare il mondo senza farsi scrupoli, cui sua madre Catherine ha dato un nome: Crudelia.
+        Catherine invita la figlia a tenere Crudelia sotto controllo, ma Estella deve trasgredire, e ad una festa organizzata dalla Baronessa
+        presso cui sua madre lavora si intrufola nel salone dove va in mostra una grande sfilata di moda. Ne nasce un putiferio, e Catherine
+        viene uccisa dai tre dalmata della Baronessa: una morte di cui Estella si ritiene colpevole. Anni dopo la ritroviamo giovane donna a Londra,
+        intenta a campare di furtarelli con gli amici Jasper e Horace, ma con ancora in testa il sogno di fare la stilista: magari proprio per la celebre Baronessa.',
+    ],
+    [
+        'title' =>'The Witches',
+        'distribuited_by' =>'Warner Bros',
+        'director' =>'Robert Zemeckis',
+        'plot' =>'Rimasto orfano dopo un incidente, un bambino di otto anni va a vivere a Demopolis, in Alabama, con la nonna.
+        È il 1968, per gli afroamericani la vita non è semplice, ma per nonna e nipote il pericolo viene soprattutto dalla scoperta che le streghe
+        - creature malvagie e orribili che odiano i bambini sopra ogni cosa - sono tornate. Convinti di sfuggire alla persecuzione, si rifugiano in
+        un hotel di lusso dove lavora un loro cugino, senza sapere, però, che proprio in quel luogo sfarzoso si terrà l\'annuale raduno delle streghe.
+        E che la tremenda Strega suprema ha intenzione di trasformare tutti i bambini del mondo in topi.',
+    ],
+    [
+        'title' =>'Sul più bello',
+        'distribuited_by' =>'Eagle Pictures',
+        'director' =>'Alice Filippi',
+        'plot' =>'Marta è bruttina e sa di esserlo. Per di più soffre di una rara malattia che non le permetterà di invecchiare, e che può
+        essere scatenata da mille fattori esterni. Ma non si lascia avvilire e affronta la vita scegliendo di vedere sempre il bicchiere mezzo pieno,
+        aiutata dai suoi migliori amici Federica e Jacopo, entrambi gay, con cui vive in una sorta di simbiosi. Il suo lavoro è leggere gli annunci
+        delle offerte al microfono di un supermercato in modo così accattivante che i clienti, senza nemmeno vederla, la riempiono di richieste di appuntamenti:
+        perché Marta, per sua stessa ammissione, esprime meglio il proprio sex appeal dietro un microfono, o dietro una tastiera. Fino a quando l\'incontro
+        con Arturo Selva, l\'uomo più bello (e ricco) di Torino, non la costringe a confrontarsi con la realtà.',
+    ],
+];
+ var_dump($movies);
+
+?>
